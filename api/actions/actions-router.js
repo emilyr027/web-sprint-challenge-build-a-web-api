9f2aef21f83d7e07e3567cafd174b1b7fddf5857
Emily Ryan <emilyryan@Emilys-MacBook-Pro.local>
2021-01-14 21:25:37 -0600

    code grade

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,59 @@
 // Write your "actions" router here!
+const express = require('express')
+const Actions = require('./actions-model')
+const router = express.Router()
+
+const { validateActionId, validateAction } = require('../middleware/middleware')
+
+router.get('/', async (req, res, next) => {
+    try {
+        const data = await Actions.get()
+        res.status(200).json(data)
+    } catch(err) {
+        next(err)
+    }
+})
+
+router.get('/:id', validateActionId, async (req, res) => {
+    res.status(200).json(req.action)
+  })
+
+router.post('/', validateAction, (req, res) => {
+    const actionInfo = req.body
+    Actions.insert(actionInfo)
+    .then((action) => {
+      res.status(201).json(action)
+    })
+    .catch((err) => {
+      res.status(500).json({ message: 'error'})
+    })
+  });
+
+router.put('/:id', validateActionId, async (req, res, next) => {
+    try {
+        const { id } = req.params
+        const data = req.body
+        if(Object.keys(data).length > 0) {
+            const edits = await Actions.update(id, data)
+            res.status(200).json(edits)
+        } else {
+            res.status(400).json({ message: 'more data needed to edit action'})
+        }
+    } catch(err) {
+        next(err)
+    }
+})
+
+router.delete('/:id', validateActionId, async (req, res, next) => {
+    try {
+        const { id } = req.params
+        const data = await Actions.remove(id)
+        res.status(200).json({ 
+            message: `Action ${id} has been deleted`, 
+            delete: data})
+    } catch(err) {
+        next(err)
+    }
+})
+
+module.exports = router
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,70 @@
 // Write your "projects" router here!
+const express = require('express')
+const Projects = require('./projects-model')
+const router = express.Router()
+
+const { validateProjectId } = require('../middleware/middleware')
+
+router.get('/', async (req, res, next) => {
+    try {
+        const data = await Projects.get()
+        res.status(200).json(data)
+    } catch(err) {
+        next(err)
+    }
+})
+
+router.get('/:id', validateProjectId, async (req, res) => {
+    res.status(200).json(req.project)
+  })
+
+router.get('/:id/actions', validateProjectId, async (req, res, next) => {
+    try {
+        const { id } = req.params
+        const data = await Projects.getProjectActions(id)
+        res.status(200).json(data)
+    } catch(err) {
+        next(err)
+    }
+  })
+
+router.post('/', async (req, res, next) => {
+    if (!req.body.name || !req.body.description) {
+        res.status(400).json({ message: 'missing name or description'})
+    } else {
+        try {
+           const data = await Projects.insert(req.body)
+           res.status(201).json(data) 
+        } catch(err) {
+            next(err)
+        }
+    }
+  })
+
+router.put('/:id', validateProjectId, async (req, res, next) => {
+    try {
+        const { id } = req.params
+        const data = req.body
+        if(Object.keys(data).length > 0) {
+            const edits = await Projects.update(id, data)
+            res.status(200).json(edits)
+        } else {
+            res.status(400).json({ message: 'more data needed to edit project'})
+        }
+    } catch(err) {
+        next(err)
+    }
+})
+
+router.delete('/:id', validateProjectId, async (req, res, next) => {
+    try {
+        const { id } = req.params
+        const data = await Projects.remove(id)
+        res.status(200).json({ 
+            message: `Project ${id} has been deleted`, 
+            delete: data})
+    } catch(err) {
+        next(err)
+    }
+})
+module.exports = router
